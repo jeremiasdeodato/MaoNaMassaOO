0344400bdf74b54d7afbdfce02534b97df12a1c6
jeremiasdeodato <jeremias.deodato30@gmail.com>
2021-12-16 21:12:03 -0300

    start inicial projeto mão na massa OO.

---
+++ mao-na-massa-oo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mao_na_massa_oo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
