97da2d05ad20e7b6fe3da73db64c552862054498
jeremiasdeodato <jeremias.deodato30@gmail.com>
2021-12-16 21:55:46 -0300

    mão na massa, c# OO

--- mao-na-massa-oo/Program.cs
+++ mao-na-massa-oo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Balta.ContentContext;
 
 namespace mao_na_massa_oo
 {
@@ -7,6 +8,9 @@ namespace mao_na_massa_oo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var course = new Course();
+
         }
     }
 }
